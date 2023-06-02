422ca710641d923d6d7decec1ad5a6be59781e7d
Taejoeng Kwak <tjkwak@pntbiz.com>
2023-06-02 17:42:04 +0900

    initialization2

--- gratines/src/App.js
+++ gratines/src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-      
+      <p>hi</p>
     </div>
   );
 }
---
+++ gratines/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFoddpLfQEeKDQj3UvG9UXLCUsX_DBiuU",
+  authDomain: "gratia-2cdd0.firebaseapp.com",
+  projectId: "gratia-2cdd0",
+  storageBucket: "gratia-2cdd0.appspot.com",
+  messagingSenderId: "136156569760",
+  appId: "1:136156569760:web:f1d37bb4aca89f8fe5e9c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
