b6fecfe6f7d810df39dbc551fba8f05a3f46f323
Taejoeng Kwak <74890485+tjKwak@users.noreply.github.com>
2023-06-06 13:17:07 +0900

    isLoggedIn에 auth값 넣기

--- gratines/src/components/App.js
+++ gratines/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import AppRouter from "./Router";
+import fbase from "../fbase";
+import { getAuth } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const auth = getAuth();
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
