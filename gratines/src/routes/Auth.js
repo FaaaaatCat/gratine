3c5d80009edcc2d1ba5f02c1d37298391d93c862
Taejoeng Kwak <74890485+tjKwak@users.noreply.github.com>
2023-06-06 14:19:14 +0900

    로그인창 만들기 전

--- gratines/src/routes/Auth.js
+++ gratines/src/routes/Auth.js
@@ -1,4 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if(name ==="email"){
+            setEmail(value)
+        }
+        else if (name === "password"){
+            setPassword(value)
+        }
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <button>Log In</button>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+            </div>
+        </div>
+    )
+}
 export default Auth;
