8630916b372fe8bb05959de8fc0e2d988d4b0dc2
Taejoeng Kwak <74890485+tjKwak@users.noreply.github.com>
2023-06-06 11:27:06 +0900

    setting true,false

--- gratines/src/fbase.js
+++ gratines/src/fbase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFoddpLfQEeKDQj3UvG9UXLCUsX_DBiuU",
-  authDomain: "gratia-2cdd0.firebaseapp.com",
-  projectId: "gratia-2cdd0",
-  storageBucket: "gratia-2cdd0.appspot.com",
-  messagingSenderId: "136156569760",
-  appId: "1:136156569760:web:f1d37bb4aca89f8fe5e9c5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
