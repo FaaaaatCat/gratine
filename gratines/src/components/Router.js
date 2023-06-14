9c53b40a17918cff0095230523e8910a22f88f41
Taejoeng Kwak <tjkwak@pntbiz.com>
2023-06-14 13:40:41 +0900

    실시간업데이트 완료 (백엔드에서 넣으려면 createdAt 값도 넣어야함)

--- gratines/src/components/Router.js
+++ gratines/src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {HashRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
--- gratines/src/routes/Home.js
+++ gratines/src/routes/Home.js
@@ -1,25 +1,44 @@
 import React, { useEffect, useState } from "react";
 // import { getFirestore } from "firebase/firestore";
 import { dbService } from '../fbase';
-import { collection, addDoc, serverTimestamp, getDocs, query, getFirestore } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp, getDocs, query, getFirestore, onSnapshot, orderBy } from "firebase/firestore";
 
 const Home = ( {userObj} ) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const getNweets = async () => {
-        const q = query(collection(dbService, "nweets"));
-        const dbNweets = await getDocs(q);
-        dbNweets.forEach((document) => {
-            const nweetObject = {
-                ...document.data(), //...은 데이터의 내용물, 즉 spread attribute 기능임
-                id: document.id,
-            }
-            setNweets(prev => [nweetObject, ...prev]);
-        });
-    };
+    //forEach를 사용하지 않는 방법(reRender 하지 않아서 더 빨라짐)
     useEffect(() => {
-        getNweets();
+        // getNweets();
+        const q = query(
+            collection(dbService, "nweets"),
+            orderBy("createdAt", "desc")
+        );
+        onSnapshot(q, (snapshot) => {
+            const nweetArray = snapshot.docs.map((document) => ({
+                id: document.id,
+                ...document.data(), //...은 데이터의 내용물, 즉 spread attribute 기능임
+            }));
+            setNweets(nweetArray);
+        });
     }, []);
+    //Array를 먼저 만들고 forEach로 갖고오는 방법
+    // const getNweets = async () => {
+    //     const q = query(collection(dbService, "nweets"));
+    //     const dbNweets = await getDocs(q);
+    //     dbNweets.forEach((document) => {
+    //         const nweetObject = {
+    //             ...document.data(), 
+    //             id: document.id,
+    //         }
+    //         setNweets(prev => [nweetObject, ...prev]);
+    //     });
+    // };
+    // useEffect(() => {
+    //     getNweets();
+    //     dbService.collection("nweets").onSnapshot(snapshot => {
+    //         console.log('hi')
+    //     })
+    // }, []);
     // const onSubmit = (event) => {
     //     event.preventDefault();
     //     dbService.collection("nweets").add({
@@ -62,7 +81,7 @@ const Home = ( {userObj} ) => {
             <div>
                 {nweets.map((nweet) => (
                     <div key={nweet.id}>
-                        <h4>{nweet.nweet}</h4>
+                        <h4>{nweet.text}</h4>
                     </div>
                 ))}
             </div>
