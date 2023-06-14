26602ba4200f6873213d16a121b4f8ac1a827afd
Taejoeng Kwak <tjkwak@pntbiz.com>
2023-06-14 15:51:48 +0900

    코드설명 주석추가

--- gratines/src/components/Nweet.js
+++ gratines/src/components/Nweet.js
@@ -3,7 +3,8 @@ import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
-    const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+    const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`); //nweetObj.id 는 쓴 트윗의 고유 id임. userObj.id와 다름.
+    //console.log(nweetObj.id) //쓴 데이터 갯수만큼 실행 됨 그래서 모든 트윗의 id를 보여줌.
 
     const [editing, setEditing] = useState(false);
     const [newNweet, setNewNweet] = useState(nweetObj.text);
@@ -12,7 +13,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
         const ok = window.confirm("진짜 삭제할래요?");
         if (ok) {
             //await dbService.doc(`nweets/${nweetObj.id}`).delete();
-            await deleteDoc(NweetTextRef );
+            await deleteDoc(NweetTextRef);
         }
 
     }
@@ -33,26 +34,31 @@ const Nweet = ({ nweetObj, isOwner }) => {
     }
     return (
         <div>
-            {editing ? (
+            {editing ? ( //편집중인가?
                 <>
-                    <form onSubmit={onSubmit}>
-                        <input
-                            onChange={onChange}
-                            type="text"
-                            placeholder="편집할 내용을 적어주세요"
-                            value={newNweet}
-                            required />
-                        <input
-                            type="submit"
-                            value="Update Nweet"
-                        />
-                    </form>
-                    <button onClick={toggleEditing}>Cancle</button>
+                    {isOwner && //주인인가? 주인일때만 아래 항목이 보이도록
+                        <>
+                            <form onSubmit={onSubmit}>
+                                <input
+                                    onChange={onChange}
+                                    type="text"
+                                    placeholder="편집할 내용을 적어주세요"
+                                    value={newNweet}
+                                    required />
+                                <input
+                                    type="submit"
+                                    value="Update Nweet"
+                                />
+                            </form>
+                            <button onClick={toggleEditing}>Cancle</button>
+                        </>
+                    }
+     
                 </>
             ): (
                 <>
                    <h4>{nweetObj.text}</h4>
-                    {isOwner && (
+                    {isOwner && ( //주인인가? 주인일때만 편집, 삭제 버튼이 보이도록
                         <>
                             <button onClick={onDeleteClick}>Delete</button>
                             <button onClick={toggleEditing}>Edit</button>
--- gratines/src/routes/Home.js
+++ gratines/src/routes/Home.js
@@ -15,11 +15,11 @@ const Home = ( {userObj} ) => {
             orderBy("createdAt", "desc")
         );
         onSnapshot(q, (snapshot) => {
-            const nweetArray = snapshot.docs.map((document) => ({
+            const nweetArray = snapshot.docs.map((document) => ({ //snapshot : 트윗을 받을때마다 알림 받는곳. 새로운 스냅샷을 받을때 nweetArray 라는 배열을 만듬
                 id: document.id,
-                ...document.data(), //...은 데이터의 내용물, 즉 spread attribute 기능임
+                ...document.data(), //...은 데이터의 내용물, 즉 spread attribute 기능임 
             }));
-            setNweets(nweetArray);
+            setNweets(nweetArray); //nweets에 nweetArray 라는 배열을 집어 넣음. 배열엔 doc.id와 doc.data()가 있음
         });
     }, []);
     //Array를 먼저 만들고 forEach로 갖고오는 방법
@@ -37,7 +37,6 @@ const Home = ( {userObj} ) => {
     // useEffect(() => {
     //     getNweets();
     //     dbService.collection("nweets").onSnapshot(snapshot => {
-    //         console.log('hi')
     //     })
     // }, []);
     // const onSubmit = (event) => {
@@ -51,10 +50,11 @@ const Home = ( {userObj} ) => {
     const onSubmit = async (e) => {
         e.preventDefault();
         console.log(`현재 쓴 트윗:${nweet}`);
-        await addDoc(collection(dbService, "nweets"), {
-        text : nweet,
-        createdAt: serverTimestamp(),
-        creatorId: userObj.uid,
+        await addDoc(collection(dbService, "nweets"), { //addDoc은 문서를 추가하는 함수. 아래 항목을 nweets의 데이터베이스에 저장함.
+            text : nweet,
+            createdAt: serverTimestamp(),
+            creatorId: userObj.uid,
+            //nweets에 새로운 데이터를 넣고싶으면 이곳에 추가하기.
         });
         setNweet("");
     };
@@ -80,11 +80,11 @@ const Home = ( {userObj} ) => {
                 <input type="submit" value="Nweet" />
             </form>
             <div>
-                {nweets.map((nweet) => (
+                {nweets.map((nweet) => ( //map은 for과 유사함. 배열안의 값들을 다 불러와주는 기능 / 지금으로썬, nweets 배열안의 데이터(doc.id / doc.data())를 다 불러옴
                     <Nweet
                         key={nweet.id}
                         nweetObj={nweet} //id, createdAt, text 3가지 값 갖고있음
-                        isOwner={nweet.creatorId === userObj.uid} //내가 실제 주인인지
+                        isOwner={nweet.creatorId === userObj.uid} //내가 실제 주인인지 //맞으면 true 값 뱉음
                     />
                 ))}
             </div>
